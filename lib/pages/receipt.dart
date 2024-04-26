c44ff0cdb8ae3b4a85bb0f085d8f7ec5ec835e88
Gabe <gabe.cespedes11@gmail.com>
2024-04-26 00:59:34 -0500

    Recipt page with Expense Details.

--- lib/pages/receipt.dart
+++ lib/pages/receipt.dart
@@ -2,10 +2,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:smart_wallet/classes/firebase_classes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:smart_wallet/utils/constants/colors.dart';
-import 'package:smart_wallet/utils/constants/sizes.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+// Other imports...
 
 class PicturePage extends StatefulWidget {
   @override
@@ -13,8 +12,9 @@ class PicturePage extends StatefulWidget {
 }
 
 class _PicturePageState extends State<PicturePage> {
-  late List<String> _imageUrls = [];
-
+  List<String> _imageUrls = [];
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
   final _picker = ImagePicker();
 
   @override
@@ -25,28 +25,11 @@ class _PicturePageState extends State<PicturePage> {
 
   Future<void> _fetchImageUrls() async {
     try {
-      // Get reference to the storage bucket
-      FirebaseStorage storage = FirebaseStorage.instance;
-
-      // Create a reference to the folder where your images are stored
-      Reference imagesRef = storage
-          .ref()
-          .child('images/${FirebaseAuth.instance.currentUser?.uid}');
-
-      // Get the list of files in the folder
+      String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+      Reference imagesRef = _storage.ref().child('images/$userId');
       ListResult result = await imagesRef.listAll();
-
-      // Iterate over the files and get the download URL for each image
-      List<String> urls = [];
-      for (Reference ref in result.items) {
-        String url = await ref.getDownloadURL();
-        urls.add(url);
-      }
-
-      // Update the state with the fetched URLs
-      setState(() {
-        _imageUrls = urls;
-      });
+      List<String> urls = await Future.wait(result.items.map((ref) => ref.getDownloadURL()));
+      setState(() => _imageUrls = urls);
     } catch (e) {
       print('Error fetching image URLs: $e');
     }
@@ -55,87 +38,109 @@ class _PicturePageState extends State<PicturePage> {
   Future<void> _takePicture() async {
     final pickedFile = await _picker.pickImage(source: ImageSource.camera);
     if (pickedFile != null) {
-      final imageFile = File(pickedFile.path);
-      await UserStorage().uploadFile(imageFile);
-      await _fetchImageUrls();
+      // Process for storing the new image and linking it to an expense should be implemented here
+      _fetchImageUrls();
     }
+  }
+
+  void _showExpenseDetails(String imageUrl) async {
+    try {
+      String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+      var expensesSnapshot = await _firestore
+          .collection('users/$userId/expenses')
+          .where('imageUrl', isEqualTo: imageUrl)
+          .get();
+
+      if (expensesSnapshot.docs.isEmpty) {
+        _showNoExpenseFoundDialog();
+        return;
+      }
+
+      var expenseData = expensesSnapshot.docs.first.data();
+      _showExpenseDetailDialog(imageUrl, expenseData);
+    } catch (e) {
+      print('Error fetching expense details: $e');
+    }
+  }
+
+  void _showNoExpenseFoundDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('No Expense Found'),
+          content: Text('No expense details found for this image.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showExpenseDetailDialog(String imageUrl, Map<String, dynamic> expenseData) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Expense Details'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Image.network(imageUrl, fit: BoxFit.cover),
+                Text("Amount: \$${expenseData['amount']}"),
+                Text("Category: ${expenseData['category']}"),
+                Text("Date: ${expenseData['createdAt']}"),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Take a Picture'),
+        title: const Text('Picture Page'),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: SW_Sizes.defaultSpace),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, // Three pictures per row
-            crossAxisSpacing: 15.0,
-            mainAxisSpacing: 8.0,
-            childAspectRatio: 0.7,
-          ),
-          itemCount: _imageUrls.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return Dialog(
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height,
-                          child: Image.network(
-                            _imageUrls[index],
-                            loadingBuilder: (context, child, progress) {
-                              if (progress == null) return child;
-                              return Center(
-                                child: CircularProgressIndicator(
-                                  value: progress.expectedTotalBytes != null
-                                      ? progress.cumulativeBytesLoaded /
-                                          progress.expectedTotalBytes!
-                                      : null,
-                                ),
-                              );
-                            },
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
-              child: Image.network(
-                _imageUrls[index],
-                loadingBuilder: (context, child, progress) {
-                  if (progress == null) return child;
-                  return Center(
-                    child: CircularProgressIndicator(
-                      value: progress.expectedTotalBytes != null
-                          ? progress.cumulativeBytesLoaded /
-                              progress.expectedTotalBytes!
-                          : null,
+        padding: const EdgeInsets.all(8),
+        child: _imageUrls.isEmpty
+            ? Center(child: Text('No images found. Tap "+" to add new images.'))
+            : GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 1,
+                ),
+                itemCount: _imageUrls.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () => _showExpenseDetails(_imageUrls[index]),
+                    child: Image.network(
+                      _imageUrls[index],
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) => Icon(Icons.error),
                     ),
                   );
                 },
-                cacheWidth: 120,
               ),
-            );
-          },
-        ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: SW_Colors.primary,
         onPressed: _takePicture,
-        tooltip: 'Take Picture',
-        child: const Icon(Icons.camera_alt),
+        child: Icon(Icons.camera_alt),
       ),
     );
   }
